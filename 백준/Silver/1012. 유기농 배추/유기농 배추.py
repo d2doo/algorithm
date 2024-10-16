aeaa3745bbc9b4d2c5c8703dadd8b8474a278b6e
d2doo <139303218+d2doo@users.noreply.github.com>
2024-10-16 20:29:32 +0900

    [Silver II] Title: 유기농 배추, Time: 148 ms, Memory: 114016 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,42 +1,40 @@
-# 유기농 배추
-# 왜 visited[ni][nj] == 0, visited = [[0] * M for _ in range(N)]
-# 이라고 하면 안돼?
-# 0이나 False나 그게 그거자나
+from collections import deque
 
-import sys
-sys.setrecursionlimit(100000)
+t = int(input())
 
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
+def bfs(i, j): # 세로랑 가로
+    q = deque()
+    di = [-1, 1, 0, 0]
+    dj = [0, 0, -1, 1]
 
-def dfs(x, y):
-    global visited
-    visited[y][x] = True
-    for num in range(4):
-        nx, ny = x + dx[num], y + dy[num]
-        if nx < 0 or M <= nx or ny < 0 or N <= ny: # 벗어나면 걸러
-            continue
-        if field[ny][nx] and not visited[ny][nx]:
-            dfs(nx, ny)
+    q.append([i, j])
+    while q:
+        i, j = q.popleft()
 
-T = int(input())
+        for k in range(4):
+            ni, nj = i + di[k], j + dj[k]
 
-for tc in range(T):
-    # 가로길이 M, 세로길이 N, 배추 개수 K
-    M, N, K = map(int, input().split())
-    cabbage = [list(map(int, input().split())) for _ in range(K)]
-    field = [[0] * M for _ in range(N)]
-    visited = [[False] * M for _ in range(N)]
+            if ni < 0 or ni > n or nj < 0 or nj > m:
+                continue
 
-    for i in range(K): # field 채우기
-        # [X, Y] == [1, 0]
-        x, y = cabbage[i]
-        field[y][x] = 1
+            if field[ni][nj] == 1:
+                q.append([ni, nj])
+                field[ni][nj] = 0
 
+for _ in range(t):
+    m, n, k = map(int, input().split()) # 가로, 세로, 배추 위치 개수
+    cabbage = [list(map(int, input().split())) for _ in range(k)]
+    field = [[0] * (m + 1) for _ in range(n + 1)]
     cnt = 0
-    for x in range(M):
-        for y in range(N):
-            if field[y][x] and not visited[y][x]: # 지금 방문한 지점이 첫 방문지라면?
-                dfs(x, y) # 거기서 상하좌우로 있는애들 다 연결해서 방문자목록에 넣어버려
+
+    for j, i in cabbage:
+        field[i][j] = 1
+
+    for i in range(n):
+        for j in range(m):
+            if field[i][j] == 1:
+                field[i][j] = 0
+                bfs(i, j)
                 cnt += 1
+                
     print(cnt)
