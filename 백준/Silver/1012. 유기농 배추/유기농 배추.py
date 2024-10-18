0d53dd792360d69804ccd07e8535d439026d00a9
d2doo <139303218+d2doo@users.noreply.github.com>
2024-10-18 21:03:12 +0900

    [Silver II] Title: 유기농 배추, Time: 148 ms, Memory: 114016 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -2,39 +2,39 @@ from collections import deque
 
 t = int(input())
 
-def bfs(i, j): # 세로랑 가로
+def bfs(x, y):
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
     q = deque()
-    di = [-1, 1, 0, 0]
-    dj = [0, 0, -1, 1]
 
-    q.append([i, j])
+    q.append([x, y])
     while q:
-        i, j = q.popleft()
+        j, i = q.popleft()
 
         for k in range(4):
-            ni, nj = i + di[k], j + dj[k]
+            ni, nj = i + dy[k], j + dx[k]
 
-            if ni < 0 or ni > n or nj < 0 or nj > m:
+            if ni > n or ni < 0 or nj > m or nj < 0:
                 continue
 
-            if field[ni][nj] == 1:
-                q.append([ni, nj])
-                field[ni][nj] = 0
+            if ground[ni][nj] == 1:
+                ground[ni][nj] = 0
+                q.append([nj, ni])
 
 for _ in range(t):
-    m, n, k = map(int, input().split()) # 가로, 세로, 배추 위치 개수
+    m, n, k = map(int, input().split()) # 가로, 세로, 배추개수
     cabbage = [list(map(int, input().split())) for _ in range(k)]
-    field = [[0] * (m + 1) for _ in range(n + 1)]
-    cnt = 0
+    ground = [[0] * (m + 1) for _ in range(n + 1)]
+    ans = 0
 
-    for j, i in cabbage:
-        field[i][j] = 1
+    for x, y in cabbage:
+        ground[y][x] = 1
 
-    for i in range(n):
-        for j in range(m):
-            if field[i][j] == 1:
-                field[i][j] = 0
-                bfs(i, j)
-                cnt += 1
-                
-    print(cnt)
+    for y in range(n):
+        for x in range(m):
+            if ground[y][x] == 1:
+                ground[y][x] = 0
+                bfs(x, y)
+                ans += 1
+
+    print(ans)
